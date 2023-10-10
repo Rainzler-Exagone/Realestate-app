51db4712a6fa66080f8a95d891213c5954b6f7fe
Rainzler Exagone <Yasser458891@gmail.com>
2023-10-10 23:46:15 +0200

    Complete

---
+++ app/MockData.js
@@ -0,0 +1,47 @@
+ export const data = [
+    {
+        id: 1,
+        img: "https://th.bing.com/th/id/R.923b5892236f2953d8c22ae7f7e3e707?rik=6zbVtbcKB7qu8g&pid=ImgRaw&r=0"
+    },
+    {
+        id: 2,
+        img: "https://th.bing.com/th/id/OIP.L2rW6uH3MXwTZgHHQDwZkwHaE7?pid=ImgDet&rs=1" 
+    },
+    {
+        id: 3,
+        img: "https://th.bing.com/th/id/R.ac23909b4927fbbf62c8206fd9f1f5f2?rik=TLgPR16mfbUvyg&pid=ImgRaw&r=0" 
+    },
+    {
+        id: 4,
+        img: "https://th.bing.com/th/id/R.2fc52e6262f5b0230906384540d704a5?rik=5HBj%2b19ERlBB0g&riu=http%3a%2f%2fap.rdcpix.com%2f1745011672%2f42d7007fa524671280e54b930a29fd04l-m0l.jpg&ehk=cGSqhPmwphAi2Rg9zbhTkF6R7sJgeAIRGdisBjqFxSw%3d&risl=&pid=ImgRaw&r=0" 
+    },
+    {
+        id: 5,
+        img: "https://th.bing.com/th/id/R.680f68bcef95464beb7e307ad6c34ed2?rik=NBhvU6AX5rNZ0A&pid=ImgRaw&r=0" 
+    },
+    {
+        id: 6,
+        img: "https://th.bing.com/th/id/R.dca342ce3f8e16ab2214c68d84c24d26?rik=OmitQ1OVDKkKSA&pid=ImgRaw&r=0" 
+    },
+    {
+        id: 7,
+        img: "https://th.bing.com/th/id/OIP.NormK1_gyfD-TL1scjHzAwHaFj?pid=ImgDet&w=640&h=480&rs=1" 
+    },
+    {
+        id: 8,
+        img: "https://th.bing.com/th/id/OIP.QbjPAWVEXsBcjGqkq6LLxwHaE7?pid=ImgDet&rs=1" 
+    },
+    {
+        id: 9,
+        img: "https://media01.findrentals.com/rentals/14996/151218/1-full.jpeg" 
+    },
+    {
+        id: 10,
+        img: "https://s3.amazonaws.com/media.savvytours.com/photos/2377251/image/9zVhEH_84487_bpo_84487_102_web_scaled_8x.jpg" 
+    },
+    {
+        id: 11,
+        img: "https://www.fotofigo.com/blog/wp-content/uploads/2019/10/real-estate-realtors2-1024x703.jpeg" 
+    }
+    
+ ]
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import React from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from "react-icons/bs"
@@ -12,6 +13,7 @@ import RealEstat1 from "./../public/realestate1.jpeg"
 import RealEstat2 from "../public/realestate2.jpeg"
 import RealEstat3 from "../public/realestate3.jpeg"
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import {data} from "./MockData"
 
 
 
@@ -20,6 +22,15 @@ import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 export default function Home() {
    const [darkMode, setDarkMode] = useState(false);
    
+   const slideLeft = () =>{
+    var slider = document.getElementById('Slider')
+    slider.scrollLeft= slider.scrollLeft - 500
+   }
+
+   const slideRight = () =>{
+    var slider = document.getElementById('Slider')
+    slider.scrollLeft= slider.scrollLeft + 500;
+   }
   return (
     <div className='relative bg-[url("../public/pexels-the-lazy-artist-gallery-1642125.jpg")]'>
       <main className='overscroll-contain ' id='r' >
@@ -50,8 +61,23 @@ export default function Home() {
         </div> 
         <div className='mt-40 text-7xl font-Lilita absolute text-white indent-8 text-center' id='Welcome'>Welcome to the Best Rental Agencie around
         </div> 
-        <div className=' absolute grid grid-flow-col gap-10 lg:flex-row mx-14 bottom-0'>
-          <IoIosArrowBack color='white' size={50} className='mt-24 cursor-pointery' />
+        <div className=' absolute flex items-center lg:flex-row mx-14 bottom-0'>
+
+
+          <IoIosArrowBack color='white' size={50} className=' cursor-pointer' onClick={slideLeft} />
+          <div className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide' id='Slider'>
+          {data.map((item)=>
+          (
+            <img 
+              className="w-[220px] aspect-4/3 rounded-2xl object-cover inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300" 
+              src={item.img} 
+              alt='/' 
+            />
+          ))}
+          </div>
+          <IoIosArrowForward  color='white' size={40} className=' cursor-pointer' onClick={slideRight} />
+          
+          {/* 
           <div className='basis-1/3 flex-1 mt-6'>
             <Image src={RealEstat1}  className='rounded-lg object-cover aspect-4/3 '></Image>
           </div>
@@ -65,7 +91,7 @@ export default function Home() {
           <Image src={RealEstat3}  className='rounded-lg object-cover aspect-4/3  cursor-pointer'></Image>
           </div>
           
-          <IoIosArrowForward  color='white' size={50} className='mt-24 cursor-pointer'  />
+           */}
         </div>
         </div>
        </section>

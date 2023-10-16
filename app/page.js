44d735778615f060e6f030dde410b7a62ff5e924
Rainzler Exagone <Yasser458891@gmail.com>
2023-10-16 23:50:22 +0200

    Complete

--- app/page.js
+++ app/page.js
@@ -6,12 +6,7 @@ import {BsFillMoonStarsFill} from "react-icons/bs"
 import example from "../public/blob-haikei.svg"
 import { useState } from 'react'
 import "./globals.css"
-import Img1 from "../public/Beluga.jpg"
-import pexels from "../public/RealEstate.jpg"
 import RealEstate from "./../public/pexels-the-lazy-artist-gallery-1642125.jpg"
-import RealEstat1 from "./../public/realestate1.jpeg"
-import RealEstat2 from "../public/realestate2.jpeg"
-import RealEstat3 from "../public/realestate3.jpeg"
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import {data} from "./MockData"
 
@@ -19,8 +14,11 @@ import {data} from "./MockData"
 
 
 
+
+
+
 export default function Home() {
-   const [darkMode, setDarkMode] = useState(false);
+   const [darkMode, setDarkMode] = useState(true);
    
    const slideLeft = () =>{
     var slider = document.getElementById('Slider')
@@ -31,11 +29,12 @@ export default function Home() {
     var slider = document.getElementById('Slider')
     slider.scrollLeft= slider.scrollLeft + 500;
    }
+   
   return (
-    <div className='relative bg-[url("../public/pexels-the-lazy-artist-gallery-1642125.jpg")]'>
+    <div className={ darkMode ? 'dark' : ""}>
       <main className='overscroll-contain ' id='r' >
         {/* Navbar */}
-       <nav className='bg-gray-100 sticky' id='nav'>
+       <nav className='bg-gray-100 sticky  ' id='nav'>
         <div className=' ml-10 mx-auto px-6 py-5 flex justify-between'>
         <div className=' flex text-5xl font-bold text-black ' id='logo'>
           <p className='text-orange-400 ' id='Oasis'>Oasis</p>
@@ -59,43 +58,36 @@ export default function Home() {
         <div id='RealEstate' className='w-full h-auto  sticky filter brightness-50 ' >
         <Image src={RealEstate}id='Image' className='  m-30 relative w-full ' ></Image> 
         </div> 
-        <div className='mt-40 text-7xl font-Lilita absolute text-white indent-8 text-center' id='Welcome'>Welcome to the Best Rental Agencie around
+        <div className='mt-52 text-7xl font-Lilita absolute text-white indent-8 text-center' id='Welcome'>Welcome to the Best Real Estate Agencie around
         </div> 
         <div className=' absolute flex items-center lg:flex-row mx-14 bottom-0'>
 
 
           <IoIosArrowBack color='white' size={50} className=' cursor-pointer' onClick={slideLeft} />
-          <div className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide' id='Slider'>
+          
+          <div className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide ' id='Slider'>
           {data.map((item)=>
-          (
+          ( 
             <img 
-              className="w-[220px] aspect-4/3 rounded-2xl object-cover inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300" 
+              className="w-[220px] aspect-4/3  rounded-2xl object-cover inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300" 
               src={item.img} 
               alt='/' 
             />
           ))}
           </div>
-          <IoIosArrowForward  color='white' size={40} className=' cursor-pointer' onClick={slideRight} />
           
-          {/* 
-          <div className='basis-1/3 flex-1 mt-6'>
-            <Image src={RealEstat1}  className='rounded-lg object-cover aspect-4/3 '></Image>
-          </div>
-          <div className='basis-1/3 flex-1 mt-6'>
-          <Image src={RealEstat2}  className='rounded-lg object-cover aspect-4/3'></Image>
-          </div>
-          <div className='basis-1/3 flex-1 mt-6'>
-          <Image src={RealEstat3}  className='rounded-lg object-cover aspect-4/3'></Image>
-          </div>
-          <div className='basis-1/3 flex-1 mt-6 ' id='H'>
-          <Image src={RealEstat3}  className='rounded-lg object-cover aspect-4/3  cursor-pointer'></Image>
-          </div>
-          
-           */}
+          <IoIosArrowForward color='white' size={40} className=' cursor-pointer' onClick={slideRight} />
         </div>
         </div>
        </section>
       </main>
+      {/* <footer className='mt-auto bg-gray-700 text-white overflow-hidden'>
+            <p>Adipisicing est ea laboris laborum proident in. Do sint in proident qui minim pariatur dolor elit ea ullamco amet veniam sit.
+             Proident ea occaecat cillum elit et velit nulla voluptate fugiat reprehenderit eiusmod ex in elit. 
+             Et ut veniam ex voluptate dolor magna do id commodo. Nostrud laboris labore consectetur occaecat commodo magna 
+             voluptate sit labore amet eu non proident commodo. Ipsum velit ullamco tempor elit adipisicing sint ex reprehenderit. 
+             In enim aute nisi do do consectetur ea sunt fugiat nulla dolor reprehenderit pariatur dolor.</p>
+      </footer> */}
     </div>
   )
 }
